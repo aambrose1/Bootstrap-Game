e671b9ff73bee0ac0b76d44e5f73c57fb8c1937f
mbrobinson1538 <94062405+mbrobinson1538@users.noreply.github.com>
2025-03-06 12:59:45 -0600

    Add files via upload

--- bootstrap/Scenes/Player/player.gd
+++ bootstrap/Scenes/Player/player.gd
@@ -3,9 +3,19 @@ extends CharacterBody2D
 @export var SPEED = 300.0
 @export var JUMP_VELOCITY = -400.0
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var player_screen_notifier = $PlayerOnScreen
+@onready var first_on_screen = false
 const bullet = preload("res://Scenes/Projectile/projectile.tscn")
 
 func _physics_process(delta: float) -> void:
+	# Once the player has been on screen at least once, we start tracking
+	if player_screen_notifier.is_on_screen():
+		first_on_screen = true
+
+	# Only quit if the player has been seen and then goes off-screen
+	if first_on_screen and not player_screen_notifier.is_on_screen():
+		get_tree().quit()
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -23,7 +33,6 @@ func _physics_process(delta: float) -> void:
 		animated_sprite.flip_h = false
 		$Marker2D.position.x = 1
 		
-		
 	if sign(direction) == -1:
 		animated_sprite.flip_h = true
 		if sign($Marker2D.position.x) == 1:
@@ -39,14 +48,9 @@ func _physics_process(delta: float) -> void:
 		get_parent().add_child(temp_proj)
 		temp_proj.position = $Marker2D.global_position
 		
-		
-
-	
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 	
-
-
 	move_and_slide()
